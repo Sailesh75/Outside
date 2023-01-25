cb9e0b6460c6d8bbf9d2404b766b230559991f05
Sailesh Kafle <saileshkafle47@gmail.com>
2023-01-25 15:52:17 +0545

    JS assignment

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,10 @@
-const domFunction=(element,className,text,targetId,source)=>{
+const domFunction=(element,className,text,targetClass,source,target)=>{
     var element=document.createElement(element);              //error if we put let
     element.className=className;
     element.appendChild(document.createTextNode(text));
-    document.querySelector(targetId).appendChild(element);   //querySelectorAll method returns a static list. Any changes made to the document after the querySelectorAll is used (like appendChild in this case) will not be reflected in the list of nodes returned.
+    document.querySelector(targetClass).appendChild(element);   //querySelectorAll method returns a static list. Any changes made to the document after the querySelectorAll is used (like appendChild in this case) will not be reflected in the list of nodes returned.
     element.href=source;
+    element.target=target;
 }
 
 //inside body
@@ -30,10 +31,9 @@ domFunction('h3','header-title','Javascript','.header2');
 for(i=1;i<7;i++){
     domFunction(`div`,`gridItem gridItem${i}`,``,`.cards1`)
 }
-for(i=1;i<4;i++){
+for(i=7;i<10;i++){
     domFunction(`div`,`gridItem gridItem${i}`,``,`.cards2`)
 }
-
 
 domFunction('h1','assignmentNumber','01','.gridItem1');
 domFunction('h3','assignmentTitle','Figma','.gridItem1');
@@ -53,12 +53,25 @@ domFunction('a','assignmentCodeLink','Links','.gridItem4','assignment_3/index.ht
 
 domFunction('h1','assignmentNumber','05','.gridItem5');
 domFunction('h3','assignmentTitle','Bootstrap assignment','.gridItem5');
-domFunction('a','assignmentCodeLink','Code','.gridItem5','https://github.com/Sailesh75/Outside-Internship/tree/master/assignment_4');
-domFunction('a','assignmentCodeLink','Demo','.gridItem5','assignment_4/index.html');
+domFunction('div','links bootstrap-link','','.gridItem5');
+domFunction('a','assignmentCodeLink','Code','.bootstrap-link','https://github.com/Sailesh75/Outside-Internship/tree/master/assignment_4','_blank');
+domFunction('a','assignmentCodeLink','Demo','.bootstrap-link','assignment_4/index.html','_blank');
 
 domFunction('h1','assignmentNumber','06','.gridItem6');
 domFunction('h3','assignmentTitle','Final project','.gridItem6');
-domFunction('a','assignmentCodeLink','Code','.gridItem6','https://github.com/Sailesh75/Outside-Internship/tree/master/Final-project');
-domFunction('a','assignmentCodeLink','Demo','.gridItem6','https://sailesh75.github.io/Outside-Internship/Final-project/dist/');
+domFunction('div','links final_project-link','','.gridItem6');
+domFunction('a','assignmentCodeLink','Code','.final_project-link','https://github.com/Sailesh75/Outside-Internship/tree/master/Final-project','_blank');
+domFunction('a','assignmentCodeLink','Demo','.final_project-link','https://sailesh75.github.io/Outside-Internship/Final-project/dist/','_blank');
 
 
+domFunction('h1','assignmentNumber','01','.gridItem7');
+domFunction('h3','assignmentTitle','Assignment1','.gridItem7');
+domFunction('div','links assignment1-link','','.gridItem7');
+domFunction('a','assignmentCodeLink','Code','.assignment1-link','https://github.com/Sailesh75/Outside-Internship/tree/master/JS','_blank');
+domFunction('a','assignmentCodeLink','Demo','.assignment1-link','https://sailesh75.github.io/Outside-Internship/','_blank');
+
+domFunction('h1','assignmentNumber','02','.gridItem8');
+domFunction('h3','assignmentTitle','Assignment1','.gridItem8');
+domFunction('div','links assignment2-link','','.gridItem8');
+domFunction('a','assignmentCodeLink','Code','.assignment2-link','https://github.com/Sailesh75/Outside-Internship/tree/master/Final-project','_blank');
+domFunction('a','assignmentCodeLink','Demo','.assignment2-link','Javascript/assignment_2/index.html','_blank');
