bea9768ff447685a6e07704c482c13a5bc0172fd
Sailesh Kafle <saileshkafle47@gmail.com>
2023-02-01 08:16:33 +0545

    car lane game additional feature update

--- Javascript/assignment_6_car_lane_game/index.js
+++ Javascript/assignment_6_car_lane_game/index.js
@@ -2,24 +2,35 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 300;
-canvas.height = 700;
+canvas.height = 750;
 
 let playerX = canvas.width / 2;
-let playerY = canvas.height - 50;
-let playerWidth = 50;
+let playerY = canvas.height - 100;
+let playerWidth = 80;
 let playerHeight = 100;
 let score = 0;
 let obstacles = [];
 let intervalId;
+let speed = 10;
+let carlane;
+let lastlane;
 
+const playerCar = new Image();
+playerCar.src = 'assests/player.png';
+
+const obstacleCar = new Image();
+obstacleCar.src = 'assests/obstacle.png'
+
+const carCollision = new Audio();
+carCollision.src = 'assests/car_crash.mp3';
 
 const createPlayer = () => {
-  ctx.fillRect(playerX - playerWidth / 2, playerY, playerWidth, playerHeight);
+  ctx.drawImage(playerCar, playerX - playerWidth / 2, playerY, playerWidth, playerHeight);
 }
 
 const createObstacles =()=> {
   obstacles.forEach(obstacle => {
-    ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+    ctx.drawImage(obstacleCar, obstacle.x, obstacle.y, obstacle.width, obstacle.height);
   });
 }
 
@@ -30,6 +41,7 @@ const checkCollision = () =>{
           playerX - playerWidth / 2 < obstacle.x + obstacle.width &&
           playerY < obstacle.y + obstacle.height
         ) {
+            carCollision.play();
             clearInterval(intervalId);
             alert("Game Over! Score: " + score);
             document.location.reload();
@@ -39,25 +51,30 @@ const checkCollision = () =>{
     }
 
 
-const updateGame = () => {
-  obstacles.forEach(obstacle => {
-    obstacle.y += 5;
-    // Removing obstacles
-    obstacles = obstacles.filter(obstacle => obstacle.y + obstacle.height <= canvas.height);
-  });
-  // Generate new obstacles
+const generateObstacles = () => {
   if (Math.random() < 0.1) {
-    let lane = Math.floor(Math.random() * 3);
-    let x = canvas.width / 3 * lane + canvas.width / 6 - 25;
-    obstacles.push({
-      x: x,
-      y: 0,
-      width: 50,
-      height: 100,
-      lane: lane
-    });
+        carlane = Math.floor(Math.random() * 3);
+    if (carlane === lastlane) {
+      carlane = (carlane + 1) % 3;
+    }
+    lastlane=carlane;
+      let x = canvas.width / 3 * carlane + canvas.width / 6 - 25;
+        if (obstacles.length<=1){
+          obstacles.push({
+            x: x,
+            y: 0,
+            width: 50,
+            height: 100,
+            carlane: carlane,
+          });
+          console.log(obstacles);
+        }
   }
   checkCollision();
+  obstacles.forEach(obstacle => {
+    obstacle.y += speed;
+    obstacles = obstacles.filter(obstacle => obstacle.y  <= canvas.height)
+  });
 }
 
 
@@ -102,11 +119,14 @@ document.onkeydown = function(e) {
     }
 };
     
-
-intervalId = setInterval(() => {
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", ()=> {
+  startButton.style.display='none';
+  intervalId = setInterval(() => {
     renderGame();
-    updateGame();
+    generateObstacles();
   }, 50);
+});
 
 
 
