40e92b4287ff62999c8f1ad922f6bfeb41e5dfa9
Sailesh Kafle <saileshkafle47@gmail.com>
2023-02-17 08:40:52 +0545

    React first assignment

---
+++ React/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Sidebar from "./session-1/assignment/dashboard/sidebar/Sidebar";
+import Screen from "./session-1/assignment/dashboard/screen/Screen";
+import './icon/variables.scss';
+
+
+function App() {
+  return (
+    <div className="dashboard">
+      <Sidebar/>
+      <Screen/>
+    </div>
+  )
+}
+
+export default App;
+
---
+++ React/vite-project/src/session-1/assignment/dashboard/screen/table/Ticket.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { MdDelete } from 'react-icons/md';
+
+const Ticket = ({ticketDetails, index, handleDelete}) =>{
+
+    return(
+
+        <tr>
+            <td>
+                <div className='ticket'>
+                    <figure>
+                        <img src={ticketDetails.customerAvatar}/>
+                    </figure>
+                    <div className='ticket_details'>
+                        <div className='ticket_items_bold'>{ticketDetails.ticketName}</div>
+                        <div className='ticket_items'>{ticketDetails.ticketInformation}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div className='ticket_items_bold'>{ticketDetails.customerName}</div>
+                <div className='ticket_items'>{ticketDetails.customerDate}</div>
+            </td>
+            <td>
+                <div className='ticket_items_bold'>{ticketDetails.dateLabel}</div>
+                <div className='ticket_items'>{ticketDetails.timeLabel}</div>
+            </td>
+            <td>
+                <div className='ticket_priority' style={{background:ticketDetails.ticketColor}}>
+                    <p>
+                        {ticketDetails.ticketPriority}
+                    </p>
+                </div>
+            </td>
+            <td>
+                <MdDelete className='ticket_deleteButton' onClick={() => handleDelete(index)}/>
+            </td>
+        </tr>
+
+    )
+}
+
+export default Ticket;
+
