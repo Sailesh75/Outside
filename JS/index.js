d206a1846d4edc9ee62be5ddd8490360a423a3b9
Sailesh Kafle <saileshkafle47@gmail.com>
2023-02-19 20:32:37 +0545

    js path added for react

--- JS/index.js
+++ JS/index.js
@@ -41,35 +41,41 @@ for(i=16;i<19;i++){
     domFunction(`div`,`gridItem gridItem${i}`,``,`.cards3`)
 }
 
+//figma
 domFunction('h1','assignmentNumber','01','.gridItem1');
 domFunction('h3','assignmentTitle','Figma','.gridItem1');
 domFunction('a','assignmentCodeLink','Link','.gridItem1','assignment_0/index.html');
 
+//html
 domFunction('h1','assignmentNumber','02','.gridItem2');
 domFunction('h3','assignmentTitle','Html','.gridItem2');
 domFunction('a','assignmentCodeLink','Links','.gridItem2','assignment_1/index.html');
 
+//css
 domFunction('h1','assignmentNumber','03','.gridItem3');
 domFunction('h3','assignmentTitle','CSS','.gridItem3');
 domFunction('a','assignmentCodeLink','Links','.gridItem3','assignment_2/index.html');
 
+//scss
 domFunction('h1','assignmentNumber','04','.gridItem4');
 domFunction('h3','assignmentTitle','SCSS','.gridItem4');
 domFunction('a','assignmentCodeLink','Links','.gridItem4','assignment_3/index.html');
 
+//bootstrap
 domFunction('h1','assignmentNumber','05','.gridItem5');
 domFunction('h3','assignmentTitle','Bootstrap','.gridItem5');
 domFunction('div','links bootstrap-link','','.gridItem5');
 domFunction('a','assignmentCodeLink','Code','.bootstrap-link','https://github.com/Sailesh75/Outside-Internship/tree/master/assignment_4','_blank');
 domFunction('a','assignmentCodeLink','Demo','.bootstrap-link','assignment_4/index.html','_blank');
 
+//frontend final project
 domFunction('h1','assignmentNumber','06','.gridItem6');
 domFunction('h3','assignmentTitle','Final project','.gridItem6');
 domFunction('div','links final_project-link','','.gridItem6');
 domFunction('a','assignmentCodeLink','Code','.final_project-link','https://github.com/Sailesh75/Outside-Internship/tree/master/Final-project','_blank');
 domFunction('a','assignmentCodeLink','Demo','.final_project-link','https://sailesh75.github.io/Outside-Internship/Final-project/dist/','_blank');
 
-
+//Javascript
 domFunction('h1','assignmentNumber','01','.gridItem7');
 domFunction('h3','assignmentTitle','Profile','.gridItem7');
 domFunction('div','links assignment1-link','','.gridItem7');
@@ -124,16 +130,17 @@ domFunction('div','links assignment9-link','','.gridItem15');
 domFunction('a','assignmentCodeLink','Code','.assignment9-link','https://github.com/Sailesh75/Outside-Internship/tree/master/Javascript/Final_project_kitti','_blank');
 domFunction('a','assignmentCodeLink','Demo','.assignment9-link','Javascript/Final_project_kitti/index.html','_blank');
 
+//React
 domFunction('h1','assignmentNumber','01','.gridItem16');
 domFunction('h3','assignmentTitle','Components, Props','.gridItem16');
 domFunction('div','links assignment10-link','','.gridItem16');
-domFunction('a','assignmentCodeLink','Code','.assignment10-link','','_blank');
+domFunction('a','assignmentCodeLink','Code','.assignment10-link','https://github.com/Sailesh75/Outside-Internship/tree/master/React/1st_assignment/vite-project','_blank');
 domFunction('a','assignmentCodeLink','Demo','.assignment10-link','https://fanciful-meerkat-a26a3f.netlify.app/','_blank');
 
 domFunction('h1','assignmentNumber','02','.gridItem17');
 domFunction('h3','assignmentTitle','useState','.gridItem17');
 domFunction('div','links assignment11-link','','.gridItem17');
-domFunction('a','assignmentCodeLink','Code','.assignment11-link','','_blank');
+domFunction('a','assignmentCodeLink','Code','.assignment11-link','https://github.com/Sailesh75/Outside-Internship/tree/master/React/2nd_assignment/vite-project','_blank');
 domFunction('a','assignmentCodeLink','Demo','.assignment11-link','https://fanciful-meerkat-a26a3f.netlify.app/','_blank');
 
 domFunction('h1','assignmentNumber','03','.gridItem18');
