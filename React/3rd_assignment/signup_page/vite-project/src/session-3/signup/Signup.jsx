c29b55a0f7c1f78e5f428b1a1a8cae9e4ccc24dd
Sailesh Kafle <saileshkafle47@gmail.com>
2023-02-23 09:05:00 +0545

    favicon added

--- React/3rd_assignment/signup_page/vite-project/src/session-3/signup/Signup.jsx
+++ React/3rd_assignment/signup_page/vite-project/src/session-3/signup/Signup.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { React } from "react";
 import PasswordToggle from "./PasswordToggle";
+import { useState } from "react";
 
 const Login = () => {
   const [PasswordInputType, ToggleIcon] = PasswordToggle();
+  const [fullName, fullNameChange] = useState("");
+  const [email, emailChange] = useState("");
+  const [password, passwordChange] = useState("");
+  const [confirmPassword, confirmPasswordChange] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Full name: ", fullName);
+    console.log("Email: ", email);
+    console.log("Password: ", password);
+    console.log("Confirm Password: ", confirmPassword);
+    fullNameChange("");
+    emailChange("");
+    passwordChange("");
+    confirmPasswordChange("");
+  };
+
   return (
     <>
       <div className="login">
@@ -13,15 +31,17 @@ const Login = () => {
           <p className="login_title">Dashboard Kit</p>
           <p className="login_text">Sign up to Dashboard Kit</p>
           <div className="form">
-            <form action="#">
+            <form onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="email">Fullname</label>
+                <label htmlFor="email">FullName</label>
                 <br />
                 <input
                   type="text"
                   name="fullname"
                   placeholder="Fullname"
                   required
+                  value={fullName}
+                  onChange={(e) => fullNameChange(e.target.value)}
                 />
               </div>
               <div style={{ marginTop: "1.2rem" }}>
@@ -32,6 +52,8 @@ const Login = () => {
                   name="email"
                   placeholder="Email address"
                   required
+                  value={email}
+                  onChange={(e) => emailChange(e.target.value)}
                 />
               </div>
               <div style={{ marginTop: "1.2rem" }}>
@@ -42,6 +64,8 @@ const Login = () => {
                   name="password"
                   placeholder="Password"
                   required
+                  value={password}
+                  onChange={(e) => passwordChange(e.target.value)}
                 />
                 <span className="password-toggle-icon">{ToggleIcon}</span>
               </div>
@@ -53,6 +77,8 @@ const Login = () => {
                   name="confirm password"
                   placeholder="Confirm Password"
                   required
+                  value={confirmPassword}
+                  onChange={(e) => confirmPasswordChange(e.target.value)}
                 />
                 <span className="password-toggle-icon">{ToggleIcon}</span>
               </div>
